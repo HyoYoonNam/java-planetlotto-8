4535bea8dbb0ecac13da7ae8ba91edd7ce888855
HyoYoonNam <dev.rudevico@gmail.com>
2026-01-10 13:19:21 +0900

    feat: setup for hyoyoonnam

--- src/main/java/planetlotto/Application.java
+++ src/main/java/planetlotto/Application.java
@@ -2,6 +2,5 @@ package planetlotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
--- src/test/java/planetlotto/ApplicationTest.java
+++ src/test/java/planetlotto/ApplicationTest.java
@@ -1,13 +1,12 @@
 package planetlotto;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -15,23 +14,23 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomUniqueNumbersInRangeTest(
-            () -> {
-                run("1000", "1,2,3,4,5", "6");
-                assertThat(output()).contains(
-                        "2개를 구매했습니다.",
-                        "[8, 11, 13, 21, 22]",
-                        "[1, 3, 6, 14, 22]",
-                        "당첨 통계",
-                        "5개 일치 (100,000,000원) - 0개",
-                        "4개 일치, 보너스 번호 일치 (10,000,000원) - 0개",
-                        "4개 일치 (1,500,000원) - 0개",
-                        "3개 일치, 보너스 번호 일치 (500,000원) - 0개",
-                        "2개 일치, 보너스 번호 일치 (5,000원) - 1개",
-                        "0개 일치 (0원) - 1개"
-                );
-            },
-            List.of(8, 11, 13, 21, 22),
-            List.of(1, 3, 6, 14, 22)
+                () -> {
+                    run("1000", "1,2,3,4,5", "6");
+                    assertThat(output()).contains(
+                            "2개를 구매했습니다.",
+                            "[8, 11, 13, 21, 22]",
+                            "[1, 3, 6, 14, 22]",
+                            "당첨 통계",
+                            "5개 일치 (100,000,000원) - 0개",
+                            "4개 일치, 보너스 번호 일치 (10,000,000원) - 0개",
+                            "4개 일치 (1,500,000원) - 0개",
+                            "3개 일치, 보너스 번호 일치 (500,000원) - 0개",
+                            "2개 일치, 보너스 번호 일치 (5,000원) - 1개",
+                            "0개 일치 (0원) - 1개"
+                    );
+                },
+                List.of(8, 11, 13, 21, 22),
+                List.of(1, 3, 6, 14, 22)
         );
     }
 
